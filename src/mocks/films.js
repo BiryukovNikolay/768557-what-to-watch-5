b7ac8a240dd7a3d2d8be12427f9dd822734c64ef
Nikolay-Biryukov <nbiryukov@kameleoon.com>
2020-10-14 21:22:10 +0300

    gitignore, films mock

---
+++ src/mocks/films.js
@@ -0,0 +1,202 @@
+export default [
+  {
+    title: `The Grand Budapest Hotel`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.9,
+    counReiewvs: 240,
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    genre: `Drama`,
+    released: 2014,
+    runTime: `1h 39m`,
+    director: [
+      `Wes Andreson,`
+    ],
+    starring: [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+      `Tony Revoloru`,
+      `Tilda Swinton`,
+      `Tom Wilkinson`,
+      `Owen Wilkinson`,
+      `Adrien Brody`,
+      `Ralph Fiennes`,
+      `Jeff Goldblum`,
+    ]
+  },
+  {
+    title: `The Lord of the Rings: The Fellowship of the Ring`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.8,
+    counReiewvs: 740,
+    description: `A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.`,
+    genre: `Adventure`,
+    released: 2001,
+    runTime: `2h 58m`,
+    director: [
+      `Peter Jackson`
+    ],
+    starring: [
+      `Sean Bean`,
+      `Cate Blanchett`,
+      `Orlando Bloom`,
+      `Billy Boyd`,
+      `Ian Holm`,
+      `Christopher Lee`,
+      `Ian McKellen`,
+      `Dominic Monaghan`,
+      `Viggo Mortensen`,
+      `John Rhys-Davies`,
+      `Liv Tyler`,
+      `Elijah Wood`,
+    ]
+  },
+  {
+    title: `Fight Club`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.8,
+    counReiewvs: 350,
+    description: `An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.`,
+    genre: `Drama`,
+    released: 1999,
+    runTime: `2h 19`,
+    director: [
+      `David Fincher`
+    ],
+    starring: [
+      `Edward Norton`,
+      `Brad Pitt`,
+      `Helena Bonham Carter`,
+      `Zach Grenier`,
+    ]
+  },
+  {
+    title: `Pulp Fiction`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.9,
+    counReiewvs: 610,
+    description: `The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.`,
+    genre: `Crime`,
+    released: 1994,
+    runTime: `2h 34m`,
+    director: [
+      `Quentin Tarantino`
+    ],
+    starring: [
+      `Tim Roth`,
+      `John Travolta`,
+      `Samuel L. Jackson`,
+      `Bruce Willis`,
+      `Ving Rhames`,
+      `Paul Calderon`,
+      `Uma Thurman`,
+      `Steve Buscemi`,
+    ]
+  },
+  {
+    title: `Sen to Chihiro no kamikakushi`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.6,
+    counReiewvs: 580,
+    description: `During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts`,
+    genre: ` Animation`,
+    released: 2001,
+    runTime: `2h 5m`,
+    director: [
+      `Hayao Miyazaki`
+    ],
+    starring: [
+      `Rumi Hiiragi`,
+      `Miyu Irino`,
+      `Tatsuya Gashûin`,
+      `Ryûnosuke Kamiki`,
+      `Yumi Tamai`,
+      `Yô Ôizumi`,
+      `Bunta Sugawara`,
+    ]
+  },
+  {
+    title: `American Beauty`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.3,
+    counReiewvs: 220,
+    description: `A sexually frustrated suburban father has a mid-life crisis after becoming infatuated with his daughter's best friend.`,
+    genre: `Drama`,
+    released: 1999,
+    runTime: `2h 2m`,
+    director: [
+      `Sam Mendes`
+    ],
+    starring: [
+      `Kevin Spacey`,
+      `Annette Bening`,
+      `Thora Birch`,
+      `Mena Suvari`,
+      `Peter Gallagher`,
+      `Allison Janney`,
+      `Chris Cooper`,
+      `Sam Robards`,
+      `Ara Celi`,
+    ]
+  },
+  {
+    title: `The Social Network`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 7.7,
+    counReiewvs: 620,
+    description: `As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea, and by the co-founder who was later squeezed out of the business.`,
+    genre: `Biography`,
+    released: 2010,
+    runTime: `2h`,
+    director: [
+      `David Fincher`
+    ],
+    starring: [
+      `Jesse Eisenberg`,
+      `Andrew Garfield`,
+      `Dustin Fitzsimons`,
+      `Rooney Mara`,
+      `Armie Hammer`,
+      `Justin Timberlake`,
+    ]
+  },
+  {
+    title: `There Will Be Blood`,
+    video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+    poster: ``,
+    wallpaper: ``,
+    rating: 8.2,
+    counReiewvs: 920,
+    description: `A story of family, religion, hatred, oil and madness, focusing on a turn-of-the-century prospector in the early days of the business.`,
+    genre: `Drama`,
+    released: 2007,
+    runTime: `2h 38m`,
+    director: [
+      `Paul Thomas Anderson`
+    ],
+    starring: [
+      `Daniel Day-Lewis`,
+      `Barry Del Sherman`,
+      `Paul Dano`,
+      `Coco Leigh`,
+      `David Willis`,
+      `David Warshofsky`,
+    ]
+  },
+];
